3927ec2c8607152b1e82a3c0e07abfee7648ec5d
Amit Gupta <amitgupta4263@gmail.com>
2026-01-04 17:45:45 +0000

    24add

---
+++ src/Patterns/24.java
@@ -0,0 +1,18 @@
+package Patterns;
+
+public class 24 {
+    public static void main(String[] args) {
+        int size=5;
+        for(int i=0;i<=i;i++){
+           for(int j= 1;j<=size;j++){
+            if(i==j || i==size-1 || j==i || j==size-1){
+                System.out.print("*");
+            }else{
+                System.out.print(" ");
+
+           }
+           System.out.println();
+        }
+        }
+    }
+}
