ebad128621fbce4ac6e098cb4ec4ff0e5f281be1
Amit Gupta <your@email.com>
2026-03-21 21:37:05 +0530

    LearnComparableAndComparetor add some funvction for makeing somthing creazy

---
+++ src/array/LearnArrayExample.java
@@ -0,0 +1,15 @@
+package array;
+
+public class LearnArrayExample {
+    public static void main(String[] args) {
+
+
+        // Declare and initialize array
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Access elements
+        System.out.println(numbers[0]); // 10
+        System.out.println(numbers[2]); // 30
+    }
+
+}
