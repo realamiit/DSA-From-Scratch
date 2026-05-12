01218eeef39d50738750550fd32b760c86061bfe
Amit Gupta <amitgupta4263@gmail.com>
2026-05-13 00:20:15 +0530

    Add initial implementation of qna01 class for recursive function demonstration

---
+++ src/LearnRecursion/QnaInRecursion/qna01.java
@@ -0,0 +1,18 @@
+package LearnRecursion.QnaInRecursion;
+
+// Print Name Using Recursion n time also
+public class qna01 {
+    public static void func(int i , int n) {
+        if (i > n) {
+            return;
+        }
+        System.out.println("Hellow Amit!!");
+        func(i + 1, n);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(n);
+        func(1, n);
+    }
+}
