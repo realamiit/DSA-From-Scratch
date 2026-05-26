1d0f37742fcf3f752d08bbc2a4d5831716e90318
Amit Gupta <amitgupta4263@gmail.com>
2026-05-27 00:03:37 +0530

    syntex error

--- src/DSAMathBasics/gcd.java
+++ src/DSAMathBasics/gcd.java
@@ -4,13 +4,13 @@ public class gcd {
     public static int main(int n1,  int n2){
      
 
-     for(int i= (n1,n2) ; i>= 1; i--){
+     for(int i= (n1,n2) ; i>= 1; i--)
 
-        if(n1 % i ==0 && n2 % i ==0){
+        if(n1 % i ==0 && n2 % i ==0)
             System.out.println(i);
-        }break;
+        break;
 
-     }
+     
     }
 
 }
