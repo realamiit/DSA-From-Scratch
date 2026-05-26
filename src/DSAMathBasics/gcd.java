d8b7c103094fc588c13262ddd2ba7e9cc813994e
Amit Gupta <amitgupta4263@gmail.com>
2026-05-26 16:20:23 +0530

    add command line for factor for uncommit line als

--- src/DSAMathBasics/gcd.java
+++ src/DSAMathBasics/gcd.java
@@ -1,10 +1,10 @@
 package DSAMathBasics;
 
 public class gcd {
-    public static void main(String[]args){
-     int n1 = 20,n2=40;
+    public static int main(int n1,  int n2){
+     
 
-     for(int i= n1,n2 ; i>= 1; i--){
+     for(int i= (n1,n2) ; i>= 1; i--){
 
         if(n1 % i ==0 && n2 % i ==0){
             System.out.println(i);
@@ -15,4 +15,7 @@ public class gcd {
 
 }
 
+// The GCD of two numbers is the largest number that divides both of them without leaving a remainder. 
+// We iterate through all numbers from 1 up to the minimum of the two input numbers, checking if each number is a common factor of both input numbers.
+// if we find two num and the  20,40 the factor is 20 cause the higest comon factor in this question is 20 because this output is bigger than fommon is 20
 
